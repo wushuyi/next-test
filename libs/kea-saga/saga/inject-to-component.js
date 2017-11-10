a553b2a99cf55db619c283ca42346c8d6a2ae0c2
wushuyi <wushuyi541@gmail.com>
2017-11-10 18:37:44 +0800

    add

--- libs/kea-saga/saga/inject-to-component.js
+++ libs/kea-saga/saga/inject-to-component.js
@@ -1,42 +1,58 @@
-import { select } from 'redux-saga/effects'
+import {select, take, put} from 'redux-saga/effects';
 
-import { startSaga, cancelSaga } from './saga'
+import {startSaga, cancelSaga} from './saga';
 
-import createSaga from './create-saga'
-import createCombinedSaga from './create-combined'
+import createSaga from './create-saga';
+import createCombinedSaga from './create-combined';
 
-import { getCache } from 'libs/kea'
+import {getCache} from 'libs/kea';
 
-const DEBUG = false
+const DEBUG = false;
 
-export default function injectSagasIntoClass (Klass, input, output) {
-  const connectedActions = output.connected ? output.connected.actions : {}
+export default function injectSagasIntoClass(Klass, input, output) {
+  const connectedActions = output.connected ? output.connected.actions : {};
 
   if (Klass.prototype._injectedKeaSaga) {
-    console.error(`[KEA] Error! Already injected kea saga into component "${(Klass && Klass.name) || Klass}"`)
+    console.error(`[KEA] Error! Already injected kea saga into component "${(Klass && Klass.name) || Klass}"`);
   }
-  Klass.prototype._injectedKeaSaga = true
+  if (Klass.getInitialProps) {
+    const originalGetInitialProps = Klass.getInitialProps;
+    Klass.getInitialProps = function (ctx) {
+      // console.log('component getInitialProps!', ctx);
+      startSaga(function* () {
+        yield take('increment (pages.index)1');
+        yield put({
+          type: 'increment (pages.index)',
+          payload: {
+            amount: 5
+          }
+        });
+        yield console.log('run startSaga!');
+      });
+      originalGetInitialProps(ctx);
+    };
+  }
+  Klass.prototype._injectedKeaSaga = true;
 
-  const originalComponentDidMount = Klass.prototype.componentWillMount
-  console.log(Klass);
+  const originalComponentDidMount = Klass.prototype.componentWillMount;
   Klass.prototype.componentWillMount = function () {
     if (DEBUG) {
-      console.log('component did mount')
+      console.log('component did mount');
     }
 
     // this === component instance
-    this._keaSagaBase = {}
-    this._keaRunningSaga = null
+    this._keaSagaBase = {};
+    this._keaRunningSaga = null;
 
-    const key = input.key ? input.key(this.props) : null
-    const path = input.path(key)
+    const key = input.key ? input.key(this.props) : null;
+    const path = input.path(key);
 
     let sagas = (input.sagas || []).map(saga => {
-      return saga && saga._keaPlugins && saga._keaPlugins.saga && saga.saga ? saga.saga : saga
-    })
+      return saga && saga._keaPlugins && saga._keaPlugins.saga && saga.saga ? saga.saga : saga;
+    });
 
     if (input.start || input.stop || input.takeEvery || input.takeLatest) {
-      const _component = this
+      const _component = this;
       _component._keaSagaBase = {
         start: input.start,
         stop: input.stop,
@@ -46,62 +62,62 @@ export default function injectSagasIntoClass (Klass, input, output) {
         key: key,
         path: path,
         props: this.props,
-        get: function * (key) {
-          const { selectors, selector } = getCache(path)
-          return yield select(key ? selectors[key] : selector)
+        get: function* (key) {
+          const {selectors, selector} = getCache(path);
+          return yield select(key ? selectors[key] : selector);
         },
-        fetch: function * () {
-          let results = {}
-          const keys = Array.isArray(arguments[0]) ? arguments[0] : arguments
+        fetch: function* () {
+          let results = {};
+          const keys = Array.isArray(arguments[0]) ? arguments[0] : arguments;
           for (let i = 0; i < keys.length; i++) {
-            results[keys[i]] = yield _component._keaSagaBase.get(keys[i])
+            results[keys[i]] = yield _component._keaSagaBase.get(keys[i]);
           }
-          return results
+          return results;
         }
-      }
+      };
 
-      let sagaActions = Object.assign({}, connectedActions)
+      let sagaActions = Object.assign({}, connectedActions);
 
       // inject key to the payload of created actions
       Object.keys(output.actions || {}).forEach(actionKey => {
         if (key) {
           sagaActions[actionKey] = (...args) => {
-            const createdAction = output.actions[actionKey](...args)
-            return Object.assign({}, createdAction, { payload: Object.assign({ key: key }, createdAction.payload) })
-          }
-          sagaActions[actionKey].toString = output.actions[actionKey].toString
+            const createdAction = output.actions[actionKey](...args);
+            return Object.assign({}, createdAction, {payload: Object.assign({key: key}, createdAction.payload)});
+          };
+          sagaActions[actionKey].toString = output.actions[actionKey].toString;
         } else {
-          sagaActions[actionKey] = output.actions[actionKey]
+          sagaActions[actionKey] = output.actions[actionKey];
         }
-      })
+      });
 
-      const saga = createSaga(this._keaSagaBase, { actions: sagaActions })
-      sagas.push(saga)
+      const saga = createSaga(this._keaSagaBase, {actions: sagaActions});
+      sagas.push(saga);
     }
 
     if (sagas.length > 0) {
-      this._keaRunningSaga = startSaga(createCombinedSaga(sagas, path.join('.')))
+      this._keaRunningSaga = startSaga(createCombinedSaga(sagas, path.join('.')));
     }
 
-    originalComponentDidMount && originalComponentDidMount.bind(this)()
-  }
+    originalComponentDidMount && originalComponentDidMount.bind(this)();
+  };
 
-  const originalComponentWillReceiveProps = Klass.prototype.componentWillReceiveProps
+  const originalComponentWillReceiveProps = Klass.prototype.componentWillReceiveProps;
   Klass.prototype.componentWillReceiveProps = function (nextProps) {
-    this._keaSagaBase.props = nextProps
+    this._keaSagaBase.props = nextProps;
 
-    originalComponentWillReceiveProps && originalComponentWillReceiveProps.bind(this)(nextProps)
-  }
+    originalComponentWillReceiveProps && originalComponentWillReceiveProps.bind(this)(nextProps);
+  };
 
-  const originalComponentWillUnmount = Klass.prototype.componentWillUnmount
+  const originalComponentWillUnmount = Klass.prototype.componentWillUnmount;
   Klass.prototype.componentWillUnmount = function () {
     if (DEBUG) {
-      console.log('component will unmount')
+      console.log('component will unmount');
     }
     if (this._keaRunningSaga) {
-      cancelSaga(this._keaRunningSaga)
+      cancelSaga(this._keaRunningSaga);
     }
 
-    originalComponentWillUnmount && originalComponentWillUnmount.bind(this)()
-  }
+    originalComponentWillUnmount && originalComponentWillUnmount.bind(this)();
+  };
 }
